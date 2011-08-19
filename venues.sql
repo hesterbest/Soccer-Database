3bba1539e0a17c7e1d2d5da1520ca3165aa8c837
Magnus Pedersen <magnop@online.no>
2011-08-19 10:40:03 +0200

    Added data

--- venues.sql
+++ venues.sql
@@ -5,3 +5,14 @@ CREATE TABLE wc2010_venues (
 	capacity INTEGER UNSIGNED NOT NULL,
 	PRIMARY KEY (id)
 ) ENGINE=INNODB;
+
+INSERT INTO venues VALUES(2, "Soccer City", "Johannesburg", 94700);
+INSERT INTO venues VALUES(3, "Moses Mabhida Stadium", "Durban", 70000);
+INSERT INTO venues VALUES(4, "Cape Town Stadium", "Cape Town", 69070);
+INSERT INTO venues VALUES(5, "Ellis Park Stadium", "Johannesburg", 62567);
+INSERT INTO venues VALUES(6, "Loftus Versfeld Stadium", "Pretoria", 51760);
+INSERT INTO venues VALUES(7, "Nelson Mandela Bay", "Port Elizabeth", 48000);
+INSERT INTO venues VALUES(8, "Free State Stadium", "Bloemfontein", 48000);
+INSERT INTO venues VALUES(9, "Peter Mokaba Stadium", "Polokwane", 46000);
+INSERT INTO venues VALUES(10, "Mbombela Stadium", "Nelspruit", 43500);
+INSERT INTO venues VALUES(11, "Royal Bafokeng Stadium", "Rustenburg", 42000);
